89c3b211c8815bb29c2809d66afb8c73d7b51c70
liveflower2021 <85013347+liveflower2021@users.noreply.github.com>
2023-06-28 09:34:53 +0300

    Add files for revision

---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+class TestProductPage:
+    
+    def test_button_add_to_basket_is_visible(self, browser):
+        browser.get(link)
+        
+        time.sleep(5)
+        
+        assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
